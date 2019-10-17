737ca129a77bb25e4ba05bf73a18ee12b17f26c2
Cassandra sloan <cassandra.sloan16@gmail.com>
2019-10-17 11:25:34 +1100

    added associations to model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,4 @@
 class Image < ApplicationRecord
   belongs_to :imagable, polymorphic: true
+  
 end
--- db/migrate/20191017002032_create_ingredients_milkshakes.rb
+++ db/migrate/20191017002032_create_ingredients_milkshakes.rb
@@ -1,7 +1,7 @@
 class CreateIngredientsMilkshakes < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredients_milkshakes do |t|
-      t.refrences :milkshake
+      t.references :milkshake, foreign_key: true
       t.references :ingredient, foreign_key: true
 
       t.timestamps
