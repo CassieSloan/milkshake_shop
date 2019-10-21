d3ed9610e520a962b31b8b98e84a86622c23076c
Cassandra sloan <cassandra.sloan16@gmail.com>
2019-10-21 11:35:19 +1100

    added active storage and removed images table

--- app/models/milkshake.rb
+++ app/models/milkshake.rb
@@ -1,6 +1,6 @@
 class Milkshake < ApplicationRecord
     has_many :ingredients_milkshakes
     has_many :ingredients, through: :ingredients_milkshakes
-    has_one :image, as: :imageable
     validates :name, :price, presence: true
+    has_one_attached :pic
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_235335) do
+ActiveRecord::Schema.define(version: 2019_10_21_003417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,15 +34,6 @@ ActiveRecord::Schema.define(version: 2019_10_20_235335) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "images", force: :cascade do |t|
-    t.string "imageable_type"
-    t.bigint "imageable_id"
-    t.string "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["imageable_type", "imageable_id"], name: "index_images_on_imageable_type_and_imageable_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
