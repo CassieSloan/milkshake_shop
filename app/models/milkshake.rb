7c1713330fc9116f3bcf7cd2a4277ff9f1038cfb
Cassandra sloan <cassandra.sloan16@gmail.com>
2019-10-21 12:08:26 +1100

    added images to each miklshake through active storage

--- app/models/milkshake.rb
+++ app/models/milkshake.rb
@@ -2,5 +2,6 @@ class Milkshake < ApplicationRecord
     has_many :ingredients_milkshakes
     has_many :ingredients, through: :ingredients_milkshakes
     validates :name, :price, presence: true
+    validates :price, numericality: { greater_than: 10 }
     has_one_attached :pic
 end
